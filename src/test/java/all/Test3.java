561e57ef4950d8e4f1af6fcd0fb61fb08c91ca2d
kras <unite699@gmail.com>
2022-01-11 17:42:33 +0200

    Pass

--- src/test/java/all/Test3.java
+++ src/test/java/all/Test3.java
@@ -1,9 +1,15 @@
+package all;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Test3 {
 
-    @Test
-    public void test3() {
-        System.out.println("test_3");
+        @Test
+        public void test3() {
+            System.out.println("Pass");
+            Assert.assertEquals("one", "one");
+
+        }
     }
-}
+
