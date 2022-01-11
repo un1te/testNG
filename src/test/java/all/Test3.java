f35ed47d2816ceaa218eb1d214b7ef904c523655
kras <unite699@gmail.com>
2022-01-11 18:36:30 +0200

    Ignored

--- src/test/java/all/Test3.java
+++ src/test/java/all/Test3.java
@@ -2,6 +2,7 @@ package all;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -10,6 +11,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class Test3  {
 
         @Test
+        @Ignore
         public void test3() {
             System.out.println("Fail");
             Assert.assertEquals("one", "two");
@@ -17,6 +19,7 @@ public class Test3  {
         }
 
     @Test (groups = {"smoke"})
+    @Ignore
     public void googleSearch2() {
         open("http://google.com");
         $(By.name("q")).setValue("johny");
